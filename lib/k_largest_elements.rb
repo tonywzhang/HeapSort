792b7b0c08a7feee159393e01804e75f9e5704a4
Tony <tonywzhang@gmail.com>
2018-09-06 11:14:48 -0700

    hw completed

--- lib/k_largest_elements.rb
+++ lib/k_largest_elements.rb
@@ -1,6 +1,8 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-  array.sort!.reverse!
-  array[0..k-1].reverse!
+  # array.sort!.reverse!
+  # array[0..k-1].reverse!
+
+  array.heap_sort!.reverse![0..k-1].reverse!
 end
